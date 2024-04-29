45ca35ef14be5040f99be6c837c3783bc6d14c5b
aidar <a.amrin@cerebra.kz>
2024-04-29 18:51:55 +0500

    implement batched inference for StyleCLIP GD

--- src/models/styleclip/global_directions/inferencer.py
+++ src/models/styleclip/global_directions/inferencer.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from src.models.base import BaseInferencer
 from src.models.styleclip.global_directions.manipulate import Manipulator
+from src.models.styleclip.global_directions.utils import GetBoundary, GetDt
 
 
 class StyleCLIPInferencer(BaseInferencer):
@@ -12,12 +13,16 @@ class StyleCLIPInferencer(BaseInferencer):
         stylegan2_path: str,
         relevance_matrix_path: str,
         clip_ckpt: str,
-        device: torch.device
+        device: torch.device,
+        alpha: float = 1,
+        beta: float = 0.1
     ):
         super().__init__()
         self.device = device
         self.manipulator = Manipulator(device=device)
-        
+        self.manipulator.alpha = [alpha]
+        self.beta = beta
+
         self.clip_model, _ = clip.load(clip_ckpt, device=device, jit=False)
 
         self.manipulator.G = self.manipulator.LoadModel(stylegan2_path, device)
@@ -28,7 +33,19 @@ class StyleCLIPInferencer(BaseInferencer):
         np.set_printoptions(suppress=True)
 
         self.fs3 = np.load(relevance_matrix_path)
-    
-    def __call__(self, images, text_prompt: str, neutral_text: str = 'face'):
-        
-    
+
+    def __call__(self, w_latents, text_prompt: str, neutral_text: str = 'face'):
+        self.manipulator.num_images = w_latents.shape[0]
+
+        s_latents = self.manipulator.G.synthesis.W2S(w_latents)
+        s_latents = self.manipulator.S2List(s_latents)
+
+        classnames = [text_prompt, neutral_text]
+        dt = GetDt(classnames, self.clip_model)
+
+        boundary, _ = GetBoundary(self.fs3, dt, self.manipulator,
+                                  threshold=self.beta)
+        codes = self.manipulator.MSCode(s_latents, boundary)
+        out = self.manipulator.GenerateImg(codes)
+
+        return out.squeeze(1)
