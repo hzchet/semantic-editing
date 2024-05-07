54de32b7c7ad607d961ce04845ec6a9d3db79d91
aidar <a.amrin@cerebra.kz>
2024-05-07 15:06:36 +0500

    change inferencer API

--- src/models/base.py
+++ src/models/base.py
@@ -2,5 +2,8 @@ from abc import ABC
 
 
 class BaseInferencer(ABC):
-    def __call__(self, images, text_prompt: str, *args, **kwargs):
+    def __call__(self, w_latent, s_latent, c_latent, text_prompt: str, *args, **kwargs):
+        raise NotImplementedError
+
+    def to_image(self, w_latent, s_latent, c_latent, *args, **kwargs):
         raise NotImplementedError
